463fa01b0648d4f134175716e5481d319aa2f8b2
Edyt Daulay <chris.stone97@gmail.com>
2023-08-08 17:21:06 +0700

    commit more model

--- admin/src/main/java/com/bootcamp/model/Company.java
+++ admin/src/main/java/com/bootcamp/model/Company.java
@@ -2,6 +2,8 @@ package com.bootcamp.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.bootcamp.base.BaseEntity;
@@ -22,6 +24,61 @@ public class Company extends BaseEntity {
     @Column(name = "company_tax_number", nullable = false)
     private String companyTaxNumber;
 
-    @Column(name = "company_photo_id", nullable = false)
-    private File file;
+    @OneToOne
+    @JoinColumn(name = "file_id", nullable = false)
+    private File companyLogo;
+
+    @OneToOne
+    @JoinColumn(name = "file_id", nullable = false)
+    private File companyBanner;
+
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyDesc() {
+        return companyDesc;
+    }
+
+    public void setCompanyDesc(String companyDesc) {
+        this.companyDesc = companyDesc;
+    }
+
+    public String getCompanyTaxNumber() {
+        return companyTaxNumber;
+    }
+
+    public void setCompanyTaxNumber(String companyTaxNumber) {
+        this.companyTaxNumber = companyTaxNumber;
+    }
+
+    public File getCompanyLogo() {
+        return companyLogo;
+    }
+
+    public void setCompanyLogo(File companyLogo) {
+        this.companyLogo = companyLogo;
+    }
+
+    public File getCompanyBanner() {
+        return companyBanner;
+    }
+
+    public void setCompanyBanner(File companyBanner) {
+        this.companyBanner = companyBanner;
+    }
+
+    
 }
