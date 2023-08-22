0f95e16d052e0c19c27b4ca34ee8acda2fd931f2
Zaahid <muhammadzaahid24@gmail.com>
2023-08-22 08:54:14 +0700

    Update request mapping and add GetMapping annotation

--- admin/src/main/java/com/lawencon/admin/controller/JobLevelController.java
+++ admin/src/main/java/com/lawencon/admin/controller/JobLevelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,13 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "bearerAuth")
 @RestController
-@RequestMapping("admin/applicant")
+@RequestMapping("admin/job-level")
 public class JobLevelController {
  
     @Autowired
     JobLevelService jobLevelService;
 
+    @GetMapping
     public ResponseEntity<List<JobLevelResDto>> getAll(){
         List<JobLevelResDto> response = jobLevelService.getAll();
 
