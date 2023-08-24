64566d967dc3e6bc005eed1cdd7a1579adc92989
Zaahid <muhammadzaahid24@gmail.com>
2023-08-24 17:22:31 +0700

    Update Security Config

--- admin/src/main/java/com/lawencon/admin/config/SecurityConfig.java
+++ admin/src/main/java/com/lawencon/admin/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 	public List<RequestMatcher> matchers() {
 		final List<RequestMatcher> matchers = new ArrayList<>();
 		matchers.add(new AntPathRequestMatcher("/admin/login", HttpMethod.POST.toString()));
-		matchers.add(new AntPathRequestMatcher("/files/**", HttpMethod.GET.toString()));
+		matchers.add(new AntPathRequestMatcher("/admin/file/**", HttpMethod.GET.toString()));
 		matchers.add(new AntPathRequestMatcher("/swagger-ui/**", HttpMethod.GET.toString()));
 		matchers.add(new AntPathRequestMatcher("/v3/**", HttpMethod.GET.toString()));
 		matchers.add(new AntPathRequestMatcher("/admin/candidate/seeker", HttpMethod.POST.toString()));
